f3fa67031bd6b0228fb0dd73fe5400785a01d7ad
thangquan <doraemona1k44@gmail.com>
2022-02-25 14:37:06 +0700

    a

--- app/components/QRCode/QRCode.js
+++ app/components/QRCode/QRCode.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+  Alert,
+  View
+} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import { Dimensions } from "react-native";
 
 const QRCode = () => {
+  const onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      Alert.alert('Thông báo', 'Quét thành công nhưng bị lỗi gì đó..')
+    );
+  };
   return (
-    <View>
-      <Text>QRCode</Text>
+    <View style={styles.container}>
+      <QRCodeScanner
+        onRead={onSuccess}
+        // flashMode={RNCamera.Constants.FlashMode.torch}
+        reactivate={true}
+                  showMarker={true} 
+        topContent={
+          <Text style={styles.centerText}>
+            Quét mã QR để sử dụng combo rửa xe
+          </Text>
+        }
+        topViewStyle={{height:0,padding:0}}
+      />
     </View>
   )
 }
 
 export default QRCode
-
-const styles = StyleSheet.create({})
+const  {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    // padding: 16
+  }
+});
